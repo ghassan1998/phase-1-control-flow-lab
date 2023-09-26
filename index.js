353cab2cb3b31dbaec2a6d4f60ac899be5dd7239
Ghassan Ajouri <nghassan1998@gmail.com>
2023-09-26 13:14:09 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance<0){
+    distance*=-1
+  }
+  if (distance<400){
+    return "This one is on me"
+
+  }else if(400<distance<2000){
+    return 'That will be twenty bucks.'
+
+  }else if(2000<distance<2500){
+    return 'I will gladly take your thirty bucks.'
+
+  }else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  return city==="NYC"?"Ok, sounds good.":'No go.'
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip)
+  {
+    case 'generous':
+      {return "Thank you so much."}
+    case 'not as generous':
+      {return "Thank you"}
+    default :
+      'Bye.'
+  }
+}
+console.log(switchOnCharmFromTip("generous"))
